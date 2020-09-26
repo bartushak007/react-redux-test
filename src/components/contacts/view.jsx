4f7dec5e1fac674e7d89d0091b0a9616592f007b
Roman Bartushak <rmnbrtshk@gmail.com>
2020-09-26 15:24:30 +0300

    finish cards

--- src/components/contacts/view.jsx
+++ src/components/contacts/view.jsx
@@ -1,20 +1,46 @@
 import React, { useMemo } from "react";
 import PropTypes from "prop-types";
-import { Card, Row, Col, Pagination } from "antd";
+import { Card, Row, Col, Pagination, Avatar } from "antd";
+import { NATIONALITIES } from "constants/nationalities";
 
 const View = ({ contacts, onChangePage }) => {
 	const { data, info } = contacts;
 	const renderCards = useMemo(
 		() =>
-			data.map(({ id, picture, email }) => (
-				<Col xs={24} sm={12} md={8} lg={4} key={id.value + email}>
-					<Card
-						hoverable
-						style={{ width: "100%", marginBottom: "24px" }}
-						cover={<img alt="contact" src={picture.large} />}
-					></Card>
-				</Col>
-			)),
+			data.map(
+				({
+					id,
+					picture,
+					email,
+					gender,
+					name,
+					registered,
+					phone,
+					nat,
+				}) => (
+					<Col xs={24} sm={12} md={8} lg={6} key={id.value + email}>
+						<Card
+							hoverable
+							style={{ width: "100%", marginBottom: "24px" }}
+							cover={<img alt="contact" src={picture.large} />}
+						>
+							<Card.Meta
+								avatar={<Avatar src={picture.medium} />}
+								title={`${name.first} ${name.last}`}
+								description={gender}
+							/>
+							<p>Age: {registered.age} </p>
+							<p>Phone: {phone}</p>
+							<p>{email}</p>
+							<p>
+								{NATIONALITIES[nat]
+									? NATIONALITIES[nat].name
+									: nat}
+							</p>
+						</Card>
+					</Col>
+				)
+			),
 		[data]
 	);
 
@@ -22,13 +48,13 @@ const View = ({ contacts, onChangePage }) => {
 		<div className={"contacts"}>
 			<Row gutter={24}>{renderCards}</Row>
 			<div className={"contacts__paginationWrapper"}>
-			<Pagination
-				current={info.page}
-				onChange={onChangePage}
-				pageSize={info.results}
-				total={100 * info.results}
-				pageSizeOptions={[12,24,36]}
-			/>
+				<Pagination
+					current={info.page}
+					onChange={onChangePage}
+					pageSize={info.results}
+					total={100 * info.results}
+					pageSizeOptions={[12, 24, 36]}
+				/>
 			</div>
 		</div>
 	);
