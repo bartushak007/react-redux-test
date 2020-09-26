20e18bda9f40a66706a740cf12be5df4568924c1
Roman Bartushak <rmnbrtshk@gmail.com>
2020-09-26 15:52:00 +0300

    Updates

---
+++ precache-manifest.2c860a35d87a1e80f6a9061f367c6d22.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d635601a989f30bc146d917a47da3af",
+    "url": "/react-redux-test/index.html"
+  },
+  {
+    "revision": "324501f4d3141a3c8269",
+    "url": "/react-redux-test/static/css/2.5c600ed6.chunk.css"
+  },
+  {
+    "revision": "584051db99e8a15d1021",
+    "url": "/react-redux-test/static/css/main.4e6f4edb.chunk.css"
+  },
+  {
+    "revision": "324501f4d3141a3c8269",
+    "url": "/react-redux-test/static/js/2.2ee86d7c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-redux-test/static/js/2.2ee86d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "584051db99e8a15d1021",
+    "url": "/react-redux-test/static/js/main.92bbfaba.chunk.js"
+  },
+  {
+    "revision": "6899450ff96d00a9c6ae",
+    "url": "/react-redux-test/static/js/runtime-main.8b5dad26.js"
+  },
+  {
+    "revision": "f7eab148df329432a159f8a1cccb102f",
+    "url": "/react-redux-test/static/media/backdrop.f7eab148.jpg"
+  },
+  {
+    "revision": "324b96ae75a3810e840babcae038bea4",
+    "url": "/react-redux-test/static/media/wezom-logo.324b96ae.svg"
+  }
+]);
